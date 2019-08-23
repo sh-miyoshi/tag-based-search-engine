0c699a97b21b354be3a738b83c7c84fe94d6ed12
Shunsuke Miyoshi <swfft814@gmail.com>
2019-08-23 15:53:14 +0900

    add register method base

---
+++ pkg/engine/register.go
@@ -0,0 +1,10 @@
+package engine
+
+// Register method register a file to database if not exists
+func Register(registerFile string, databaseFile string) error {
+	// TODO
+	// Read DB file
+	// if not exists
+	//   register data
+	return nil
+}
